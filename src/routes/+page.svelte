291b06378adc2a699f621a718bf38e1d635dad5a
EnoHakami <faisalHakami16@gmail.com>
2023-12-20 23:38:42 +0300

    added scss breakpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,10 @@
     h1 {
         color: red;
         font-size: functions.toRem(36);
+        @include breakpoint.up('md') {
+            color: green
+        }@include breakpoint.up('lg') {
+            color: blue
+        }
     }
 </style>
