707345ca0139d47b56ad776df586d67c10b9b5b7
MuhammadHakami <engmuhammadhakami@gmail.com>
2023-12-25 22:06:28 +0300

    added the login cookies

--- src/routes/api/auth/callback/+server.ts
+++ src/routes/api/auth/callback/+server.ts
@@ -1,5 +1,6 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types'
+import { SPOTIFY_APP_CLIENT_ID, SPOTIFY_APP_CLIENT_SECRET, BASE_URL } from '$env/static/private';
 
 export const GET: RequestHandler = async ({url, cookies, fetch}) => {
     const code = url.searchParams.get('code') || null;
@@ -12,10 +13,32 @@ export const GET: RequestHandler = async ({url, cookies, fetch}) => {
         throw error(400, 'State Mismatch!')
     }
 
-//     const response = await fetch('https://accounts.spotify.com/api/tokens',{
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/x-www-form-urlencoded",
-//         }
-//     })
-// };
+    const response = await fetch('https://accounts.spotify.com/api/token',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${Buffer.from(
+                `${SPOTIFY_APP_CLIENT_ID}:${SPOTIFY_APP_CLIENT_SECRET}`
+            ).toString('base64')}`
+        },
+        body: new URLSearchParams({
+            code: code || '',
+            redirect_uri: `${BASE_URL}/api/auth/callback`,
+            grant_type: 'authorization_code',
+            code_verifier: storedChallengeVerifier || '',
+            client_id: SPOTIFY_APP_CLIENT_ID
+        })
+    });
+    const responseJSON = await response.json();
+    
+    if (responseJSON.error) {
+        throw error(400, responseJSON.error_description);
+    }
+
+    cookies.delete('spotify_auth_state', {path: '/'});;
+    cookies.delete('spotify_auth_challenge_verifier', {path: '/'});
+    cookies.set('refresh_token', responseJSON.refresh_token, {path: '/'});
+    cookies.set('access_token', responseJSON.access_token, {path: '/'});
+    throw redirect(303, '/');
+    
+};
