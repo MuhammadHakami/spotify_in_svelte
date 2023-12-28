d0f51bed50562d139ee7f6b4c31d678817c53f47
MuhammadHakami <engmuhammadhakami@gmail.com>
2023-12-28 21:44:54 +0300

    added refresh token and expire handler

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,16 @@
 import type { LayoutServerLoad } from "./$types";
 import { SPOTIFY_BASE_URL } from "$env/static/private";
+import { redirect } from "@sveltejs/kit";
+import { url } from "inspector";
 
-export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
+export const load: LayoutServerLoad = async ({ cookies, fetch, url }) => {
     const accessToken = cookies.get('access_token');
+    const refreshToken = cookies.get('refresh_token')
     if(!accessToken) {
         return {
             user: null
         };
-    }
+    };
 
     const profileRes = await fetch(`${SPOTIFY_BASE_URL}/me`, {
         headers: {
@@ -19,10 +22,21 @@ export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
         const profile: SpotifyApi.CurrentUsersProfileResponse = await profileRes.json();
         return {
             user: profile
-        }
-    } else {
+        };
+    } if (profileRes.status === 401 && refreshToken) {
+        // refresh the token and try again
+        const refreshRes = await fetch('/api/auth/refresh');
+        if(refreshRes.ok) {
+            throw redirect(307, url.pathname)
+        };
         return {
             user: null
-        }
-    }
-}
+        };
+    } else {
+        
+    } {
+        return {
+            user: null
+        };
+    };
+};
