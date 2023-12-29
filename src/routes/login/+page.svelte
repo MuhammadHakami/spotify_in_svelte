d220649f2d0120162d84e95c02a9732bbe651e37
MuhammadHakami <engmuhammadhakami@gmail.com>
2023-12-29 23:32:00 +0300

    added login/logout scss  button styles

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,7 @@
-<a href="/api/auth/login">Login</a>
+<script lang='ts'>
+    import { Button } from '$components';
+</script>
+
+<!-- <a href="/api/auth/login">Login</a> -->
+<Button element="a" variant='outline' href="/api/auth/login">Login</Button>
+<Button element="button">Logout</Button>
