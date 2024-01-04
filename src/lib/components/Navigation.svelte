ca7c5b615068792793b299e4f527425463dad972
Faisal Hakami <faisalhakami16@gmail.com>
2024-01-04 22:05:37 +0300

    added icon

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,6 +1,8 @@
 <script lang='ts'>
     import type { ComponentType } from "svelte";
-    import { Home, Search, ListMusic, type Icon } from 'lucide-svelte'
+    import { Home, Search, ListMusic, type Icon } from 'lucide-svelte';
+    import logo from '$assets/Spotify_Logo_RGB_white.png';
+    import { page } from '$app/stores';
 
     export let desktop: boolean;
 
@@ -25,6 +27,25 @@
 
 <div class='nav-content' class:desktop class:mobile={!desktop}>
 <nav aria-label='Main'>
+    <div class ='nav-content-inner'>
+        <img src={logo} class="logo" alt="Spotify" width="100px"/>
+    </div>
+    <ul>
+        {#each menuItime as itme} 
+        <li class:active={itme.path == $page.url.pathname}>
+            <a href={itme.path}>
+                <svelte:component 
+                this={itme.icon} 
+                focusable='false' 
+                aria-hidden='true' 
+                color='var(--text-color)'
+                 size={26}
+                 strokeWidth={2}/>
+                {itme.label}
+            </a>
+        </li>
+        {/each}
+    </ul>
 
 </nav>
 </div>
