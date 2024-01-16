1787d98f38a7bc8b49bb24c5c993b8a6ff0db799
MuhammadHakami <engmuhammadhakami@gmail.com>
2024-01-16 23:09:28 +0300

    added the mobile navigation tab

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -4,7 +4,11 @@
     import type { ComponentType } from "svelte";
     import logo from '$assets/Spotify_Logo_RGB_White.png';
 	import { page } from '$app/stores';
+    import { fade } from 'svelte/transition';
     export let desktop: boolean;
+
+    let isMobileMenuOpen = false;
+    $: isOpen = desktop || isMobileMenuOpen;
 
     const menuItems: {path: string, label: string, icon: ComponentType<Icon>}[] = [
         {
@@ -22,12 +26,42 @@
             label: 'Playlists',
             icon: ListMusic
         }
-    ]
+    ];
+
+    const openMenu = () => {
+        isMobileMenuOpen = true;
+    };
+    const closeMenu = () => {
+        isMobileMenuOpen = false;
+    };
+
 </script>
 
+<svelte:head>
+    {#if !desktop && isMobileMenuOpen}
+        <style>
+            body {
+                overflow: hidden;
+            }
+        </style>
+    {/if}
+</svelte:head>
+
 <div class="nav-content" class:desktop class:mobile={!desktop}>
+    {#if !desktop && isMobileMenuOpen}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div class="overlay" on:click={closeMenu} transition:fade={{ duration: 200 }} />
+    {/if}
+
     <nav aria-label="Main">
-        <div class='nav-content-inner'>
+        {#if !desktop}
+            <button on:click={openMenu}>Open</button>
+        {/if}
+        <div class='nav-content-inner' class:is-hidden={!isOpen}>
+            {#if !desktop}
+                <button on:click={closeMenu}>Close</button>
+            {/if}
             <img src={logo} class="logo" alt="Spotify"/>
             <ul>
                 {#each menuItems as item}
@@ -47,6 +81,19 @@
 
 <style lang="scss">
     .nav-content {
+        .overlay {
+            position: fixed;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            background-color: var(--sidebar-color);
+            opacity: 0.75;
+            z-index: 100;
+            @include breakpoint.up('md') {
+                display: none;
+            }
+        }
         .logo {
             max-width: 100%;
             width: 130px;
@@ -98,6 +145,20 @@
                 @include breakpoint.up('md') {
                     display: block;
                 }
+            }
+        }
+        &.mobile .nav-content-inner {
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: 100;
+            transition: transform 200ms, opacity 200ms;
+            &.is-hidden {
+                transform: translateX(-100%);
+                opacity: 0;
+            }
+            @include breakpoint.down('md') {
+                display: block;
             }
         }
     }
